59d39d788f49b38465cdd710d953dd54eea7a861
c0d3r <strug1538@flyymail.com>
2024-01-03 06:24:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from pyrogram import Client, filters, idle
 from pyrogram.types import Message, ForceReply, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 AHBot = Client(
-    "session_string=Config.BOT_USERNAME",
+    "session_string"=Config.BOT_USERNAME,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
 )
 UserBot = Client(
-    "session_string=Config.USER_SESSION_STRING",
+    "session_string"=Config.USER_SESSION_STRING,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH
 )
