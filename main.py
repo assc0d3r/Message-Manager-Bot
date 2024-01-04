910ea113b7c0b0bf1ec657ac7a8e445af148222d
c0d3r <strug1538@flyymail.com>
2024-01-04 00:02:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ UserBot = TelegramClient(StringSession(USER_SESSION_STRING), API_ID, API_HASH)
 #)
 
 
-@AHBot.on_message(filters.command(['start', f'start@{Config.BOT_USERNAME}']))
+@AHBot.on(filters.command(['start', f'start@{Config.BOT_USERNAME}']))
 async def start_handler(bot: Client, message: Message):
     if (not await mongodb.is_chat_exist(message.chat.id)) and (message.chat.type != "private"):
         try:
@@ -63,7 +63,7 @@ async def start_handler(bot: Client, message: Message):
     )
 
 
-@AHBot.on_message(filters.command(["settings", f"settings@{Config.BOT_USERNAME}"]) & ~filters.private & ~filters.edited)
+@AHBot.on(filters.command(["settings", f"settings@{Config.BOT_USERNAME}"]) & ~filters.private & ~filters.edited)
 async def settings_handler(bot: Client, message: Message):
     user = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
     print(f"User Status: {user.status}\nCan Change Info: {user.can_change_info}")
@@ -93,7 +93,7 @@ async def settings_handler(bot: Client, message: Message):
     await show_settings(editable)
 
 
-@AHBot.on_message(filters.reply & filters.text & ~filters.private & ~filters.edited)
+@AHBot.on(filters.reply & filters.text & ~filters.private & ~filters.edited)
 async def reply_handler(bot: Client, message: Message):
     if not await mongodb.is_chat_exist(message.chat.id):
         return
@@ -134,7 +134,7 @@ async def reply_handler(bot: Client, message: Message):
         )
 
 
-@UserBot.on_message((filters.text | filters.media) & ~filters.private & ~filters.edited, group=-1)
+@UserBot.on((filters.text | filters.media) & ~filters.private & ~filters.edited, group=-1)
 async def main_handler(_, message: Message):
     if not await mongodb.is_chat_exist(message.chat.id):
         return
